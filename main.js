ab8bd42f832244654598550d017f46a1246f302c
Carlos de la Cruz <carlosdelacruzacosta4@gmail.com>
2020-11-21 12:46:44 -0600

    got rid of modules, sorry

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
-
-
 const container = document.querySelector("#master-pane");
 
-container.addEventListener('scroll', () =>{
-    console.log("sup");
-});
-   
+
+
+
+function createMasterItem(pokemon, container){
+    const article = document.createElement("article");
+    article.innerHTML = `<p><span>${pokemon.id}</span>${pokemon.name}</p>`;
+    article.classList.add("pokemon");
+    container.appendChild(article);
+  }
+  
+  async function loadMoreItems(offSet, container){
+    const pokemons = await getElements(offSet);
+    pokemons.forEach(pokemon => createMasterItem(pokemon, container));
+  }
+
+
+container.addEventListener('scroll', function() {
+    if (container.scrollTop + container.clientHeight >= container.scrollHeight) {
+      console.log("sup")
+    }
+  });
   
--- modules/getData.js
+++
@@ -1,5 +0,0 @@
-export async function getElemenst(offSet){
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offSet}&limit=20`);
-    const data = response.json();
-    return data;
-}
