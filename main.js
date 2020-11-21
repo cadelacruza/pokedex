8d3dabd161bdecfa89a0aff445955821f23dfc90
Carlos de la Cruz <carlosdelacruzacosta4@gmail.com>
2020-11-21 13:19:20 -0600

    made the request work

--- main.js
+++ main.js
@@ -2,23 +2,36 @@ const container = document.querySelector("#master-pane");
 
 
 
+//Get the data from the API to load more items when scrolling
+async function getElements(offSet){
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offSet}&limit=20`);
+    const data = await response.json();
+    return data;
+}
 
+//Create html for new Pokemon item on master pane
 function createMasterItem(pokemon, container){
     const article = document.createElement("article");
     article.innerHTML = `<p><span>${pokemon.id}</span>${pokemon.name}</p>`;
     article.classList.add("pokemon");
-    container.appendChild(article);
+    const parent =  document.querySelector("#master-pane");
+    parent.appendChild(article);
   }
   
+  //Load html of pokemons created
   async function loadMoreItems(offSet, container){
     const pokemons = await getElements(offSet);
-    pokemons.forEach(pokemon => createMasterItem(pokemon, container));
+    console.log(pokemons.results);
+    pokemons.results.forEach(pokemon => createMasterItem(pokemon));
   }
 
 
+
+//Detect when the scroll bar reaches the bottom of the master pane aka container
 container.addEventListener('scroll', function() {
     if (container.scrollTop + container.clientHeight >= container.scrollHeight) {
-      console.log("sup")
+        const offSet = container.lastChild.datasets;
+      loadMoreItems(offSet, container);
     }
   });
   
