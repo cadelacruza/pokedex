d7bf15b2a4fbb70d81c83eb38ce5214d5b1902d5
Carlos de la Cruz <carlosdelacruzacosta4@gmail.com>
2020-11-22 12:25:25 -0600

    made scroll event work

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 const container = document.querySelector("#master-pane");
 
-
-
 //Get the data from the API to load more items when scrolling
 async function getElements(offSet){
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offSet}&limit=20`);
@@ -10,9 +8,9 @@ async function getElements(offSet){
 }
 
 //Create html for new Pokemon item on master pane
-function createMasterItem(pokemon, container){
+function createMasterItem(pokemon, container, id){
     const article = document.createElement("article");
-    article.innerHTML = `<p><span>${pokemon.id}</span>${pokemon.name}</p>`;
+    article.innerHTML = `<p><span>${id}</span>${pokemon.name}</p>`;
     article.classList.add("pokemon");
     const parent =  document.querySelector("#master-pane");
     parent.appendChild(article);
@@ -20,18 +18,23 @@ function createMasterItem(pokemon, container){
   
   //Load html of pokemons created
   async function loadMoreItems(offSet, container){
+      //console.log("sup")
     const pokemons = await getElements(offSet);
-    console.log(pokemons.results);
-    pokemons.results.forEach(pokemon => createMasterItem(pokemon));
+    //console.log(pokemons.results[0]);
+    let id = container.lastElementChild.dataset.id;
+    //console.log(pokemons.results.length);
+    for(let i = 0; i < pokemons.results.length; i++){
+       id++;
+       createMasterItem(pokemons.results[i], container, id);
+    }
   }
-
 
 
 //Detect when the scroll bar reaches the bottom of the master pane aka container
 container.addEventListener('scroll', function() {
     if (container.scrollTop + container.clientHeight >= container.scrollHeight) {
         const offSet = container.lastChild.datasets;
-      loadMoreItems(offSet, container);
+        loadMoreItems(offSet, container);
     }
   });
   
